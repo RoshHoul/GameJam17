954e7a999d1a75b36ec65958d6eabb34df17ce41
RoshHoul <kabadzhg@uni.coventry.ac.uk>
2017-01-20 21:53:26 +0000

    Test Scene
    Moving Rectangle/ Platform

---
+++ Assets/Coin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    Transform myTarget;
+    GameObject coin;
+    float shootAngle = 30f;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            GameObject curCoin = Instantiate(coin, transform.position, Quaternion.identity);
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = BallisticVel(myTarget, shootAngle);
+        }
+	}
+
+    Vector2 BallisticVel(Transform target, float angle)
+    {
+        var dir = target.position - transform.position;
+        var h = dir.y;
+        dir.y = 0;
+        var dist = dir.magnitude;
+        var a = angle * Mathf.Deg2Rad;
+        dir.y = dist * Mathf.Tan(a);
+        dist += h / Mathf.Tan(a);
+
+        var vel = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+        return vel * dir.normalized;
+    }
+
+    
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private bool onFloor;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var val = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * val, rb.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {   
+            if (onFloor) 
+                rb.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            onFloor = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+            onFloor = true;
+    }
+}
