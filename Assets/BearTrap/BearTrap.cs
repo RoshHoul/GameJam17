39d03dd4c124cfa7180522059991f8cb2aa305db
mikelsoh <mikelsoh@uni.coventry.ac.uk>
2017-01-21 00:31:14 +0000

    fef
    as

---
+++ Assets/BearTrap/BearTrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour {
+	public Animator Snap;
+	public GameObject Player;
+	public GameObject EndScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (GameObject.FindGameObjectWithTag("Player"))
+		{
+			Snap.SetInteger ("States", 1);
+			Player.GetComponent<PlayerMovement> ().enabled = false;
+			EndScreen.SetActive (true);
+		}
+				
+	}
+}
