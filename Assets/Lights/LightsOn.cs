a5bd2ebe24515063707b974a16f64510aa32a742
Arnolds Manguzis <sniegs11@gmail.com>
2017-01-21 07:56:31 +0000

    bra
    breee

--- Assets/Lights/LightsOn.cs
+++ Assets/Lights/LightsOn.cs
@@ -1,52 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class LightsOn : MonoBehaviour {
 
-    public GameObject[] lights;
-    public int lightnum = 0;
-    public float counter;
-    public float counter2;
-    public bool count1;
-    public bool count2;
-    public bool forstop = true;
-    public bool for2stop = false;
-    // Use this for initialization
-    void Start()
-    {
-        //InvokeRepeating ("lightsareon", 1f,0.5f);
-    }
+	public GameObject[] lights;
+	public int lightnum = 0;
+	public int lightnum2 = 4;
+	public float counter;
+	public float counter2;
+	public bool for1on = true;
+	public bool for2on = false;
+	// Use this for initialization
+	void Start()
+	{
+		//InvokeRepeating ("lightsareon", 1f,0.5f);
+	}
 
-    void lightsareon()
-    {
+	void lightsareon()
+	{
 
-    }
-    // Update is called once per frame
-    void Update()
-    {
-/*        for (int i = 0; i <= 5; i++) {
-            Debug.Log("this is the" + i + " time");
-            counter += Time.deltaTime;
-            if (counter >= 0.3f) {
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		counter += Time.deltaTime;
+		counter2 += Time.deltaTime;
+		if (for1on == true) {
+			for (int i = 0; i <= 4; i++) {
 
-                lights[i].SetActive(true);
-                counter = 0;
-            }
+				if (counter >= 0.05f) {
+
+					lights [lightnum].SetActive (true);
+					lightnum++;
+					counter = 0;
+				}
+				if (lightnum == 5) {
+					lightnum = 6;
+				}
+				if (lightnum == 6) {
+					lightnum = 6;
+					for2on = true;
+					for1on = false;
+				}
+
+			}
 
 
 
 
-        }
-        for (int j = 5; j >= 5; j--) {
+		}
+		if (for2on == true) {
+			for (int j = 4; j >= 0; j--) {
+				if (counter2 >= 3f) {
 
-            counter2 = counter2 + Time.deltaTime;
-            if (counter2 >= 2f) {
-                lights[j].SetActive(false);
-                counter2 = 0;
-                //					forstop = false;
-            }
-        } */
-    } 
+					lights [lightnum2].SetActive (false);
+					if (lightnum2 == 0) {
+						lightnum2 = 0;
+						for2on = false;
+					}
+					lightnum2--;
+					counter = 0;
+				}
 
+
+			}
+
+		}
+	} 
 }
+
